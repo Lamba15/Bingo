ff96795eb798c9b2cc3181b5c78fd211b657b915
Lamba15 <programmer.lamba@gmail.com>
2018-01-22 22:13:30 +0200

    new image

--- app/src/main/java/tk/wave_co/bingo/MainActivity.java
+++ app/src/main/java/tk/wave_co/bingo/MainActivity.java
@@ -2,6 +2,7 @@ package tk.wave_co.bingo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        Toast.makeText(this, "ana 7tet sora boss 3leha keda", Toast.LENGTH_SHORT).show();
     }
 }
